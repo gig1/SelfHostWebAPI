292ee9ee3869dd4ac94e94de2485b90053867a78
gig1 <maksum90@yahoo.com>
2017-05-04 20:39:09 -0500

    Initial Commit

---
+++ SelfHostWebAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostWebAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/value
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/value").Result;
+
+                //Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
